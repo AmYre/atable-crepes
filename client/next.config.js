4e5cf5477ccadcf6371e9b3fa7b8aa8d26e35ea8
bensabri <ssabril@ymail.com>
2022-03-06 21:09:49 +0100

    deploy strapi to heroku

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+	images: {
+		domains: ['a-vos-crepes.herokuapp.com'],
+	},
 	reactStrictMode: true,
 	env: {
 		stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
--- client/pages/cuisine.js
+++ client/pages/cuisine.js
@@ -23,7 +23,7 @@ const Kitchen = () => {
 		setInterval(() => {
 			const timer = refetch();
 			return clearInterval(timer);
-		}, 10000);
+		}, 60000);
 		setInterval(() => {
 			const timer = timerCountDown();
 			return clearInterval(timer);

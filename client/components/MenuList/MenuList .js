3306a34ed72648dc20db0fd8abf724b54823132e
bensabri <ssabril@ymail.com>
2022-03-08 19:37:05 +0100

    fix bugs

--- client/components/MenuList/MenuList .js
+++ client/components/MenuList/MenuList .js
@@ -23,8 +23,8 @@ const FoodAndDrink = () => {
 			<h3 className="p-5 text-center text-xl font-bold">
 				FAITES VOTRE CHOIX
 			</h3>
-			<div className="h-96 mx-5 mt-2 mb-5 bg-white shadow-md overflow-hidden overflow-y-scroll">
-				<div className="m-3 my-2 p-2 bg-white">
+			<div className="h-96 mx-5 mt-2 mb-5 bg-white dark:bg-gray-800 shadow-md overflow-hidden overflow-y-scroll">
+				<div className="m-3 my-2 p-2 bg-white dark:bg-gray-800">
 					<CrepesSucrees />
 					<Boisson />
 				</div>

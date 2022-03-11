251942c9e69235ccda288c73d2227d233c29ae06
bensabri <ssabril@ymail.com>
2022-03-11 10:36:04 +0100

    fix bug

--- client/hooks/apollo/apollo-client.js
+++ client/hooks/apollo/apollo-client.js
@@ -16,7 +16,7 @@ const client = () => {
 	const link = createHttpLink({
 		// uri: 'http://localhost:1337/graphql',
 		uri: `${process.env.NEXT_PUBLIC_HOST_STRAPI}/graphql`,
-		credentials: 'include',
+		// credentials: 'include',
 		// headers: getHeader(),
 	});
 

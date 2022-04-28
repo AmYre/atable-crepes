fece935253487f0b977a4b7a78c26d0f3f08caed
bensabri <ssabril@ymail.com>
2022-04-28 17:38:25 +0000

    test

--- server/src/api/commande/controllers/commande.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  commande controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::commande.commande');
--- server/src/api/commande/services/commande.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * commande service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::commande.commande');
